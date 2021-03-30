98f994058a9c50dcc3eb1153c4a6db771aeb7a3a
Jacob Werner <jacob.scott.werner@gmail.com>
2021-03-30 11:25:11 -0500

    Created initial skeleton of project.

---
+++ MusicLibraryAPI/ActionFilters/GlobalRouting.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicLibraryAPI.ActionFilters
+{
+    public class GlobalRouting : IActionFilter
+    {
+        private readonly ClaimsPrincipal _claimsPrincipal;
+        
+        public GlobalRouting(ClaimsPrincipal claimsPrincipal)
+        {
+            _claimsPrincipal = claimsPrincipal;
+        }
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var controller = context.RouteData.Values["controller"];
+            if (controller.Equals("Home"))
+            {
+                if (_claimsPrincipal.IsInRole("Customer"))
+                {
+                    context.Result = new RedirectToActionResult("Index",
+                    "Customers", null);
+                }
+                else if (_claimsPrincipal.IsInRole("Employee"))
+                {
+                    context.Result = new RedirectToActionResult("Index",
+                    "Employees", null);
+                }
+            }
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+
+
+
+        //// GET: api/<GlobalRouting>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/<GlobalRouting>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<GlobalRouting>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<GlobalRouting>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<GlobalRouting>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
